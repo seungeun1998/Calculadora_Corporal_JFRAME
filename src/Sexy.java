4cb6515f15aefb14417c59138b569d895c81baf9
Hector <Correo>
2021-04-25 16:04:50 +0200

    Agregando clase seria

---
+++ src/Sexy.java
@@ -0,0 +1,15 @@
+
+public class Sexy {
+	private String nivelSexy;
+	
+	//Esta clase es muy sexy y su nivel de sexitura es ilimitado
+	public Sexy(String nivelSexy) {
+		super();
+		this.nivelSexy = nivelSexy;
+	}
+	 
+ 
+ 
+ 
+ 
+}
